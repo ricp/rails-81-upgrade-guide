4104f7f482546bfe4909214eecfed8ad830e2915
Richard Piacentini <ricp@users.noreply.github.com>
2025-10-08 23:43:41 +0700

    Change output directory from _site to docs for GitHub Pages compatibility

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function(eleventyConfig) {
       input: "src",
       includes: "_includes",
       data: "_data",
-      output: "_site"
+      output: "docs"
     },
     // Process .html files with Liquid template engine
     htmlTemplateEngine: "liquid"
